3221bd63a0877ed69c6dfbde77a8af49a08a5f91
Fabio Caumo <fabiovpcaumo@gmail.com>
2019-03-11 16:53:05 -0300

    problema21

---
+++ problema21.py
@@ -0,0 +1,7 @@
+lista = [1, 2, 3, 4, 5]
+meuDict = {'lista' : lista,
+            'lista ao quadrado' : [x**2 for x in lista],
+            'lista ao cubo' : [x**3 for x in lista]
+            }
+
+print (meuDict)
