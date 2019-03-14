4b861e333432ee5e4fa8ef22cb952507196de11e
fabio.caumo <fabio.caumo@dieboldnixdorf.com>
2019-03-14 10:41:14 -0300

    problema15.py

--- semana01/aula02/problema15.py
+++ semana01/aula02/problema15.py
@@ -1,10 +1,14 @@
-def soma_2(n):
-    return n + 2
+def soma_2(numero):
+    return numero + 2
 
-def subtrai_2(n):
-    return n - 2
+
+def subtrai_2(numero):
+    return numero - 2
+
 
 def twice(f, val):
     return f(f(val))
 
+
 print(twice(soma_2, 2))
+print(twice(subtrai_2, 2))
